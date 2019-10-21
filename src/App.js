ce76b8a9f38a257cbca2d05bbbd06be4d6dc83da
Herve Bourelle <herve.bourelle@gmail.fr>
2019-10-21 16:56:57 +0200

    bug react corrigé

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div id="pokemon">
+     
     </div>
-  );
-}
+    <button onClick={textJSON()} >Another</button>
+    </div>
+  )}
+  
 
 export default App;
+
+
+function fetchPokemonJSON() {
+ 
+  const url = `https://quests.wilders.dev/simpsons-quotes/quotes`;
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(pokemon) {
+      console.log('data decoded from JSON:', pokemon);
+       const pokemonHtml = `
+        <h1>${pokemon[0].character}</h1>
+        <img src="${pokemon[0].image}" />
+        <p id='text'>${pokemon[0].quote}</p>
+      `;
+      
+      
+  })
+}
+  
+fetchPokemonJSON();
+
+function textJSON() {
+  const url = `https://quests.wilders.dev/simpsons-quotes/quotes`;
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(pokemon) {
+      console.log('data decoded from JSON:', pokemon);
+
+    
+    const pokemonHtml = `
+      
+      ${pokemon[0].quote}
+    `;
+   
+  })
+}
+
